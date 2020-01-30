81af524bad8d1a7b08438eaf334e7f2114ea612f
IgorAssuncao <igor.ormonde@gmail.com>
2020-01-30 21:35:43 +0000

    created PlanetList Component and Planet Component and made a request with axios to get planets from star wars api

---
+++ src/components/PlanetComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="planet">
+    <h3>Name: {{ planet.name }}</h3>
+    <p>Climate: {{ planet.climate }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Planet',
+  props: {
+    planet: Object,
+  },
+};
+</script>
---
+++ src/components/PlanetsList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main-list">
+    <ul>
+      <li v-for="planetObj in planets" v-bind:key="planetObj.name">
+        <Planet :planet="planetObj" />
+      </li>
+    </ul>
+    <button @click="getAllPlanets">Get All Planets</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Planet from './PlanetComponent.vue';
+
+const url = 'https://swapi.co/api/planets';
+
+async function getAllPlanets() {
+  return axios.get(url);
+}
+
+export default {
+  data() {
+    return {
+      planets: [],
+    };
+  },
+  methods: {
+    async getAllPlanets() {
+      const response = await getAllPlanets();
+
+      this.planets = response.data.results;
+
+      return this.planets;
+    },
+  },
+  components: {
+    Planet,
+  },
+};
+</script>
